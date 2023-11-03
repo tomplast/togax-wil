bf368a8a812afffd471bb78d794ed30262acdb91
Tomas Gustavsson <tomplast@gmail.com>
2023-11-03 16:27:47 +0100

    WIP

--- src/togawil/__init__.py
+++ src/togawil/__init__.py
@@ -1,23 +1,58 @@
+from __future__ import annotations
 # type: ignore
 import toga, toga.style
 from typing import Dict, Iterable
 import re
-
+import uuid
+from collections import defaultdict
 LINE_SYNTAX = re.compile(r'^( *)([a-zA-Z_\?]+:) *?([a-zA-Z0-9_\? \'\"]+|) *$')
+
+
+class LineReader:
+    def __init__(self, lines):
+        self._lines = lines
+        self._line_index = 0
+
+    @property
+    def current_line_number(self):
+        return self._line_index + 1
+
+    def get_current_line(self) -> str:
+        if self._line_index >= len(self._lines):
+            return None
+
+        return self._lines[self._line_index]
+    
+    def peek_next_line(self) -> str:
+        if self._line_index+1 >= len(self._lines):
+            return None
+
+        return self._lines[self._line_index+1]
+
+
+
+    def next(self):
+        self._line_index += 1
 
 class IteratorPlusOne:
     def __init__(self, it: Iterable):
         self._it = it
         self._go_back = False
+        self._line_count = 1
 
     def go_back(self):
         self._go_back = True
+
+    @property
+    def line_count(self):
+        return self._line_count
 
     def __next__(self):
         if self._go_back:
             self._go_back = False
             return self._current_value
 
+        self._line_count += 1
         self._current_value = next(self._it)
         return self._current_value
 
@@ -34,18 +69,115 @@ def extract_line_parts(line):
     return indentation_count, key, value
 
 def load_widget_from_string(string: str) -> toga.Widget:
-    def _return_widget_instance(widget_type_name, widget_type_attributes) -> toga.Widget:
-        if 'style' in widget_type_attributes:
-            widget_type_attributes['style'] = toga.style.Pack(**widget_type_attributes['style'])
-        widget = getattr(toga, widget_type_name)(**widget_type_attributes)
-        return widget
-    def _return_widget_with_children(widget_type_name, widget_type_attributes, child_widgets: list[toga.Widget]):
-        widget = _return_widget_instance(widget_type_name, widget_type_attributes) #getattr(toga, widget_type_name)(**widget_type_attributes)
-        for child in child_widgets:
-            widget.add(child)
+    lines = LineReader([x for x in string.split('\n') if len(x) > 0])
 
-        return widget
+    def workie():
+        initial_attributes = {}
+        current_widget_name = None
+        current_widget_attributes = {}
+        current_widget_attribute = None
+        children = []
 
+        while line := lines.get_current_line():
+            current_indentation, current_key, current_value = extract_line_parts(line) 
+            is_widget = current_key[0].istitle()
+
+            if is_widget:
+                if current_widget_name:
+                    children.append(_return_widget_instance(current_widget_name, current_widget_attributes))
+
+                if initial_attributes == {}:
+                    initial_attributes = current_widget_attributes
+
+                current_widget_name = current_key
+                current_widget_attributes = {}
+                current_widget_attribute = None
+            else:
+                current_widget_attributes[current_key] = current_value
+                current_widget_attribute = current_key
+
+            next_line = lines.peek_next_line()
+            if not next_line:
+                break
+
+            next_indentation, *_ = extract_line_parts(next_line)
+
+            if next_indentation > current_indentation:
+                lines.next()
+                returned_initial_attributes, returned_children, returned_attributes, negative_jump = workie()
+                
+                if current_widget_attribute != None:
+                    current_widget_attributes[current_widget_attribute] = returned_attributes
+                    current_widget_attribute = None
+                else:
+                    current_widget_attributes.update(returned_attributes)
+
+                if returned_initial_attributes != {}:
+                    children.append(_return_widget_with_children(current_key, initial_attributes, returned_children))
+                    current_widget_name = None
+                    current_widget_attributes = {}
+                    current_widget_attribute = None
+                    returned_children = []
+                
+
+                for child in returned_children:
+                    children.append(child)
+
+                if negative_jump < 0:
+                    if current_widget_name:
+                        children.append(_return_widget_instance(current_widget_name, current_widget_attributes))
+                    else:
+                        raise NotImplementedError()
+                    return {}, children, {}, 0
+                
+                next_line = lines.peek_next_line()
+                next_indentation = current_indentation
+                if next_line != None:
+                    next_indentation, *_ = extract_line_parts(next_line)
+
+                if not next_line or (next_indentation - current_indentation) < 0:
+                    if not current_widget_name:
+                        return initial_attributes, children, current_widget_attributes, next_indentation - current_indentation
+
+                    widget = _return_widget_instance(current_widget_name, current_widget_attributes)
+                    if not widget.can_have_children:
+                        children.append(widget)
+                        return initial_attributes, children, {}, 0
+
+                    for child in children:
+                        widget.add(child)
+                    
+                    return initial_attributes, [widget], {}, 0
+
+            elif next_indentation < current_indentation:
+                return initial_attributes, children, current_widget_attributes, 0
+
+            lines.next()
+
+        if current_widget_name:
+            children.append(_return_widget_instance(current_widget_name, current_widget_attributes))
+            current_widget_name = None
+            current_widget_attributes = {}
+
+        return initial_attributes, children, current_widget_attributes, 0
+
+    res = workie()
+    return res[1][0] if type(res) is tuple else res
+
+
+def _return_widget_instance(widget_type_name, widget_type_attributes) -> toga.Widget:
+    if 'style' in widget_type_attributes:
+        widget_type_attributes['style'] = toga.style.Pack(**widget_type_attributes['style'])
+    widget = getattr(toga, widget_type_name)(**widget_type_attributes)
+    return widget
+def _return_widget_with_children(widget_type_name, widget_type_attributes, child_widgets: list[toga.Widget]):
+    widget = _return_widget_instance(widget_type_name, widget_type_attributes) #getattr(toga, widget_type_name)(**widget_type_attributes)
+    for child in child_widgets:
+        widget.add(child)
+
+    return widget
+
+def _load_widget_from_string(string: str) -> toga.Widget:
     def _parse_content(content: IteratorPlusOne, parent_indentation_count, depth=0) -> toga.Widget:
         line = None
 
@@ -89,7 +221,8 @@ def load_widget_from_string(string: str) -> toga.Widget:
             is_widget = key[0].istitle()
             if is_widget:
                 if indentation_count > parent_indentation_count:
-                    content.go_back()
+                    pass # Worked content.go_back()
+                    v = _parse_content(content, indentation_count, depth + 1)
                     additional_children, should_go_back = _parse_content(content, indentation_count, depth + 1)
 
                     for child in additional_children:
@@ -144,6 +277,7 @@ def load_widget_from_string(string: str) -> toga.Widget:
                     current_widget_type_attributes[key] = value
                     current_widget_type_attribute_key = key
 
+    line_count = count(x for x in string.split('\n') if len(x) > 0)
     lines = IteratorPlusOne(iter(x for x in string.split('\n') if len(x) > 0))
     return _parse_content(lines, 0)[0][0]
 
--- tests/test_library.py
+++ tests/test_library.py
@@ -3,6 +3,24 @@ import toga
 
 def test_load_one_level_widget_tree_from_string():
     widget_string = '''
+Box:'''
+
+    widget = load_widget_from_string(widget_string)
+    assert type(widget) is toga.Box
+
+def test_load_one_level_widget_with_attributes_from_string():
+    widget_string = '''
+Box:
+    id: 'box1'
+'''
+
+    widget = load_widget_from_string(widget_string)
+    assert type(widget) is toga.Box
+    assert widget.id == 'box1' 
+
+
+def test_load_two_level_widget_tree_from_string():
+    widget_string = '''
 Box:
     PasswordInput:
     Label:
@@ -18,13 +36,14 @@ Box:
     assert type(widget.children[1]) is toga.Label and widget.children[1].text == 'Password is "password"'
     assert type(widget.children[2]) is toga.Button and widget.children[2].text == 'Authenticate'
     
-def test_load_two_level_widget_tree_from_string():
+def test_load_three_level_widget_tree_from_string():
     widget_string = '''
 Box:
+    id: box1
     Box:
+        id: box2
         Label:
             text: 'Am I living in a box?'
-
     Label:
         text: 'Am I living in a cardboard box?'
 '''
@@ -37,13 +56,16 @@ Box:
 
     assert type(widget.children[1]) is toga.Label and widget.children[1].text == 'Am I living in a cardboard box?'
 
-def test_load_three_level_widget_tree_from_string():
+def test_load_four_level_widget_tree_from_string():
     widget_string = '''
 Box:
     Box:
+        id: 'box5'
         Box:
             Label:
                 text: 'Am I living in a box?'
+                style:
+                    padding: '50
 
     Label:
         text: 'Am I living in a cardboard box?'
@@ -62,6 +84,11 @@ Box:
     Label:
         text: 'Am I living in a box?'
         style:
+            padding: 50
+
+    Box:
+        PasswordInput:
+        style:
             padding: 50'''
 
     widget = load_widget_from_string(widget_string)
@@ -71,3 +98,21 @@ Box:
     assert widget.children[0].text == 'Am I living in a box?'
     assert widget.children[0].style.padding == (50, 50, 50, 50)
 
+
+def test_box_ception():
+    widget_string = '''
+Box:
+    Box:
+        Box:
+            Box:
+                Box:
+                    Box:
+                    Label:
+                        text: "Label"'''
+    widget = load_widget_from_string(widget_string)
+    assert type(widget) is toga.Box
+
+    child = None 
+    for i in range(0, 5):
+        child = child.children[0] if child != None else widget.children[0]
+        assert type(child) is toga.Box
